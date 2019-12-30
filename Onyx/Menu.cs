61b598317e03c22f37ed2045df41707dca370f72
Sebastian Mitchell-Berry <sebsky2@hotmail.com>
2019-12-29 22:46:54 -0800

    Finished displaying of the pause menu

--- Onyx/Menu.cs
+++ Onyx/Menu.cs
@@ -10,56 +10,148 @@ namespace Onyx
     {
         public static void Pause()
         {
-            List<(string label, int highlight)> buttons = new List<(string label, int highlight)>();
-            int[] rows = { 0, 0 };
+            bool canEscape = false;
+            List<(string label, ConsoleColor highlight)> buttons = new List<(string label, ConsoleColor highlight)>();
 
-            if (!Game.playing)
+            if (Game.playing)
             {
                 if (!Program.hasSave)
                 {
-                    buttons.Add(("Play", 0));
-                    buttons.Add(("Exit", 0));
-                    rows[0] = 2;
+                    buttons.Add((" Play", ConsoleColor.DarkGray));
+                    buttons.Add((" Exit", ConsoleColor.DarkGray));
                 }
                 else
                 {
-                    buttons.Add(("New", 0));
-                    buttons.Add(("Load", 0));
-                    buttons.Add(("Exit", 0));
-                    rows[0] = 1;
-                    rows[1] = 2;
+                    buttons.Add((" New", ConsoleColor.DarkGray));
+                    buttons.Add((" Load", ConsoleColor.DarkGray));
+                    buttons.Add((" Exit", ConsoleColor.DarkGray));
                 }
             }
             else
             {
-                if (!Program.hasSave)
+                canEscape = true;
+
+                if (Program.hasSave)
                 {
-                    buttons.Add(("Resume", 0));
-                    buttons.Add(("Save", 0));
-                    buttons.Add(("Exit", 0));
-                    rows[0] = 1;
-                    rows[1] = 2;
+                    buttons.Add(("Resume", ConsoleColor.DarkGray));
+                    buttons.Add((" Save", ConsoleColor.DarkGray));
+                    buttons.Add((" Exit", ConsoleColor.DarkGray));
                 }
                 else
                 {
-                    buttons.Add(("Resume", 0));
-                    buttons.Add(("Save", 0));
-                    buttons.Add(("Load", 0));
-                    buttons.Add(("Exit", 0));
-                    rows[0] = 2;
-                    rows[1] = 2;
+                    buttons.Add(("Resume", ConsoleColor.DarkGray));
+                    buttons.Add((" Save", ConsoleColor.DarkGray));
+                    buttons.Add((" Exit", ConsoleColor.DarkGray));
+                    buttons.Add((" Load", ConsoleColor.DarkGray));
                 }
             }
 
+            List<List<(string label, ConsoleColor highlight)>> rows = makeRows(buttons);
+
             bool selected = false;
-            (int row, int col) = (0, 0);
+            (int row, int col) selection = (0, 0);
+            (int row, int col) previousSelection = (0, 0);
 
             while (!selected)
             {
-                buttons[selection] = (buttons[selection].label, 1);
-                Screen.Draw(buttons);
+                if (selection.col > rows[selection.row].Count() - 1)
+                {
+                    selection.col = rows[selection.row].Count() - 1;
+                }
 
-                (row, col) = Game.Navigate(rows);
+                rows[previousSelection.row][previousSelection.col] = (rows[previousSelection.row][previousSelection.col].label, ConsoleColor.DarkGray);
+                rows[selection.row][selection.col] = (rows[selection.row][selection.col].label, ConsoleColor.Red);
+                previousSelection = selection;
+                Screen.Draw(rows);
+
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                if (input == ConsoleKey.UpArrow)
+                {
+                    if (selection.row == 0)
+                    {
+                        selection.row = rows.Count() - 1;
+                    }
+                    else
+                    {
+                        selection.row -= 1;
+                    }
+                }
+
+                if (input == ConsoleKey.DownArrow)
+                {
+                    if (selection.row == rows.Count() - 1)
+                    {
+                        selection.row = 0;
+                    }
+                    else
+                    {
+                        selection.row += 1;
+                    }
+                }
+
+                if (input == ConsoleKey.LeftArrow)
+                {
+                    if (selection.col == 0)
+                    {
+                        selection.col = rows[selection.row].Count() - 1;
+                    }
+                    else
+                    {
+                        selection.col -= 1;
+                    }
+                }
+
+                if (input == ConsoleKey.RightArrow)
+                {
+                    if (selection.col == rows[selection.row].Count() - 1)
+                    {
+                        selection.col = 0;
+                    }
+                    else
+                    {
+                        selection.col += 1;
+                    }
+                }
+
+                if (input == ConsoleKey.Enter)
+                {
+                    selected = true;
+                }
+
+                if (input == ConsoleKey.Escape && canEscape)
+                {
+                    selection = (0, 0);
+                    selected = true;
+                }
+            }
+
+            List<List<(string, ConsoleColor)>> makeRows(List<(string, ConsoleColor)> allButtons)
+            {
+                List<List<(string, ConsoleColor)>> buttonRows = new List<List<(string, ConsoleColor)>>();
+                List<(string, ConsoleColor)> buttonRow = new List<(string, ConsoleColor)>();
+
+                for (int b = 0; b < buttons.Count(); b++)
+                {
+                    buttonRow.Add(buttons[b]);
+
+                    if (b == 0 && buttons.Count() % 2 == 1)
+                    {
+                        List<(string, ConsoleColor)> finalRow = new List<(string, ConsoleColor)>();
+                        finalRow.AddRange(buttonRow);
+                        buttonRows.Add(finalRow);
+                        buttonRow.Clear();
+                    }
+                    else if (buttonRow.Count() == 2)
+                    {
+                        List<(string, ConsoleColor)> finalRow = new List<(string, ConsoleColor)>();
+                        finalRow.AddRange(buttonRow);
+                        buttonRows.Add(finalRow);
+                        buttonRow.Clear();
+                    }
+                }
+
+                return buttonRows;
             }
         }
     }
--- Onyx/Screen.cs
+++ Onyx/Screen.cs
@@ -14,6 +14,7 @@ namespace Onyx
         //Initializes important screen values
         public static void Initialize()
         {
+            Console.CursorVisible = false;
             currentWindowSize = (Console.WindowWidth, Console.WindowHeight);
         }
 
@@ -30,45 +31,71 @@ namespace Onyx
         }
 
         //Draw method specifically for drawing the pause menu
-        public static void Draw(List<(string label, int highlight)> buttons)
+        public static void Draw(List<List<(string label, ConsoleColor highlight)>> buttonRows)
         {
+            (int col, int row) mOrigin = (0, 0);
+            int height = 0;
+
             //Drawing the background of the pause menu
             Console.BackgroundColor = ConsoleColor.Gray;
 
-            if (buttons.Count() == 2)
+            if (buttonRows.Count() == 1)
             {
-                (int col, int row) = ((Console.WindowWidth / 2) - 10, (Console.WindowHeight / 2) - 3);
-
-                for (int r = 0; r < 3; r++)
-                {
-                    Console.SetCursorPosition(col, row + r);
-                    Console.Write("                 ");
-                }
+                mOrigin = ((Console.WindowWidth / 2) - 10, (Console.WindowHeight / 2) - 2);
+                height = 5;
             }
             else
             {
-                (int col, int row) = ((Console.WindowWidth / 2) - 10, (Console.WindowHeight / 2) -1);
-                
-                for (int r = 0; r < 7; r++)
-                {
-                    Console.SetCursorPosition(col, row + r);
-                    Console.Write("                 ");
-                }
+                mOrigin = ((Console.WindowWidth / 2) - 10, (Console.WindowHeight / 2) - 4);
+                height = 9;
             }
 
-            Console.BackgroundColor = ConsoleColor.Black;
+            for (int r = 0; r < height; r++)
+            {
+                Console.SetCursorPosition(mOrigin.col, mOrigin.row + r);
+                Console.Write("                          ");
+            }
+
             Console.ForegroundColor = ConsoleColor.White;
 
-            //If there is an odd number of buttons
-            if (buttons.Count() % 2 == 1)
+            for (int r = 0; r < buttonRows.Count(); r++)
             {
-                Console.SetCursorPosition((Console.WindowWidth / 2) - 4, (Console.WindowHeight / 2) - 2);
-                Console.Write(buttons[0]);
-            }
-            //If there is an even number of buttons
-            else
-            {
+                (int col, int row) rOrigin = (0, mOrigin.row + (4 * r) + 1);
 
+                if (buttonRows[r].Count() == 1)
+                {
+                    rOrigin.col = mOrigin.col + 8;
+                }
+                else
+                {
+                    rOrigin.col = mOrigin.col + 2;
+                }
+
+                for (int c = 0; c < buttonRows[r].Count(); c++)
+                {
+                    (int col, int row) bOrigin = (rOrigin.col + (12 * c), rOrigin.row);
+
+                    for (int br = 0; br < 3; br++)
+                    {
+                        if (br == 2)
+                        {
+                            Console.BackgroundColor = buttonRows[r][c].highlight;
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                        }
+
+                        Console.SetCursorPosition(bOrigin.col, bOrigin.row + br);
+                        Console.Write("          ");
+
+                        if (br == 1)
+                        {
+                            Console.SetCursorPosition(bOrigin.col + 2, bOrigin.row + 1);
+                            Console.Write(buttonRows[r][c].label);
+                        }
+                    }
+                }
             }
         }
     }
